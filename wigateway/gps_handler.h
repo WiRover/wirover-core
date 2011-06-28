376f0012a61b286bdceee56ae4643b96304948fc
Lance Hartung <hartung@cs.wisc.edu>
2011-06-28 22:31:18 +0000

    Added missing gps_handler.{c,h}
    git-svn-id: svn+ssh://wicontroller4/home/svn/wirover_kernel/trunk@245 ba602a36-4df9-46f2-848b-6875ca4683ef

---
+++ wigateway/gps_handler.h
@@ -0,0 +1,16 @@
+#ifndef GPS_HANDLER_H
+#define GPS_HANDLER_H
+
+// Invalidate GPS data after 5 seconds
+#define GPS_DATA_TIMEOUT    5
+
+/* Forward declarations */
+struct gps_fix_t;
+struct gps_data_t;
+struct gps_payload;
+
+int init_gps_handler();
+int fill_gps_payload(struct gps_payload *dest);
+
+#endif //GPS_HANDLER_H
+
