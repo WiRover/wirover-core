6704536ba4a5a34c9c859b9d497d7d93230b51ca
WiRover Team <wirover-comments@cs.wisc.edu>
2013-06-28 16:17:33 -0500

    Added some missing files

---
+++ wicontroller/dbq.h
@@ -0,0 +1,12 @@
+#define DBQ_LENGTH 32
+typedef struct{
+	char query[1040];
+	int gps_req;
+        int gwid;
+        char hash[41];
+        int len;
+}dbqreq;
+
+void init_dbq();
+int dbq_enqueue(dbqreq* req);
+dbqreq* dbq_dequeue();
