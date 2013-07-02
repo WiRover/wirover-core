726193e6bb05a07b624f586fd1bafb5ba8bccca5
WiRover Team <wirover-comments@cs.wisc.edu>
2013-07-02 15:44:24 -0500

    Fixed whtiespace in dbq files

--- wicontroller/dbq.h
+++ wicontroller/dbq.h
@@ -1,10 +1,10 @@
 #define DBQ_LENGTH 32
 
 typedef struct{
-	char query[1040];
-	int gps_req;
-        int gwid;
-        char hash[NODE_HASH_SIZE + 1];
+    char query[1040];
+    int gps_req;
+    int gwid;
+    char hash[NODE_HASH_SIZE + 1];
 }dbqreq;
 
 void init_dbq();
